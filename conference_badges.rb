775303c589b2b8916f7bd40efe26a625f4db2d76
Kat Maldon <katharinamaldon@gmail.com>
2019-01-19 21:04:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,7 +2,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_maker(attendees)
+def batch_badge_creator(attendees)
   attendees.collect do |name|
     badge_maker(name)
   end
