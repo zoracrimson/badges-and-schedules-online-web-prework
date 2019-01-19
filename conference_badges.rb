293afdc975979f3a059f153042131649eb76ac2c
Kat Maldon <katharinamaldon@gmail.com>
2019-01-19 21:07:05 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,3 +8,13 @@ def batch_badge_creator(attendees)
   end
 end
 
+def assign_rooms(attendees)
+  room = 0 
+  attendees.collect do |name|
+    room += 1 
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end
+
+
+    
