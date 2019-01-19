9f8f0cbed28791022e54338440feada10792b9a8
Kat Maldon <katharinamaldon@gmail.com>
2019-01-19 21:09:41 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,5 +16,14 @@ def assign_rooms(attendees)
   end
 end
 
+def printer(attendees)
+  batch_badge_creator(attendees).each do |value|
+    puts value
+  end
+  assign_rooms(attendees).each do |value|
+    puts value
+  end
+end
 
+  
     
